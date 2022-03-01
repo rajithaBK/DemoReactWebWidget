bd7c12a8f62d463d3d783a3581bc4d63b046c19d
rajitha123k <rajithabhavani.kantheti@gmail.com>
2022-03-01 14:56:19 -0500

    initial commit

---
+++ src/simple-calendar.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { HtmlTagWrapper } from './html-tag-wrapper';
+import {WebexMeetingsWidget} from '@webex/widgets';
+
+import '@webex/widgets/dist/css/webex-widgets.css';
+
+
+interface SimpleCalendarProps {
+  clickDisabled?: boolean;
+}
+
+function SimpleCalendar(props: SimpleCalendarProps) {
+
+  return (
+    <div className="WebexMeeting">
+        { 
+            <WebexMeetingsWidget
+            accessToken="NjNmNmMzN2YtMjZiMi00MGI4LTk1YjQtY2RkOTJjYTE2OWEyZDc5N2VmZWItZDRm_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
+            meetingDestination="https://cisco.webex.com/meet/rkanthet"
+          />
+        }
+    </div>
+  );
+}
+
+export { SimpleCalendar };
